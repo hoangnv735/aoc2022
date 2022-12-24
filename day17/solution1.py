fa6937c31cfe1fd37bedaa558cc8fc69ab1eb721
hoangnv735 <deserteaglexiii@gmail.com>
2022-12-24 16:59:17 +0700

    add day 17

--- day17/solution1.py
+++ day17/solution1.py
@@ -1,81 +1,154 @@
-import re
-from collections import defaultdict, deque
-from functools import lru_cache
-
-
-input_file = "example.txt"
-# input_file = "input.txt"
-
-valve_adj = defaultdict(list)
-flow_rates = {}
-
+import os
+from copy import deepcopy 
+input_file = 'example.txt'
+# input_file = 'input.txt'
 
 with open(input_file) as f:
-    for line in f:
-        line = line.strip()
-        if line != "":
-            valve = line[6:8]
-            flow_rates[valve] = int(re.search(r"(?<=rate\=)\d+", line).group(0))
+    effect_string = f.read().strip()
 
-            next_idx = line.find("valves")
-            if next_idx == -1:
-                next_idx = line.find("valve") + 6
+
+sidx = 0
+sl = len(effect_string)
+def get_effect():
+    global sidx
+    effect = effect_string[sidx]
+    sidx = (sidx + 1) % sl
+    return -1 if effect == '<' else 1
+
+rocks = [
+    {
+        # shape: bar
+        'blocks': [(0,0), (0,1), (0,2), (0,3)],
+        'height': 1,
+        'width': 4,
+    },
+    {
+        # shape: cross
+        'blocks': [(0,1), (1,0), (1,1), (1,2), (2,1)],
+        'height': 3,
+        'width': 3,
+    },
+    {
+        # shape: L
+        'blocks': [(0,0), (0,1), (0,2), (1,2), (2,2)],
+        'height': 3,
+        'width': 3,
+    },
+    {
+        # shape: column
+        'blocks': [(0,0), (1,0), (2,0), (3,0)],
+        'height': 4,
+        'width': 1,
+    },
+    {
+        # shape: square
+        'blocks': [(0,0), (0,1), (1,0), (1,1)],
+        'height': 2,
+        'width': 2,
+    },
+]
+ridx = 0
+rl = len(rocks)
+def get_rock():
+    global ridx
+    rock = rocks[ridx]
+    ridx = (ridx + 1) % rl
+    return rock
+
+
+max_height = 0
+pg = []
+for _ in range(3):
+    row = [0] * 7
+    pg.append(row)
+
+
+def get_pos():
+    y = 2
+    x = max_height + 3
+    return x, y
+
+
+def valid(rock, pos):
+    # right wall
+    if pos[1] + rock['width'] > 7:
+        return False
+    # left wall
+    if pos[1] < 0: 
+        return False
+    # bottom
+    if pos[0] < 0:
+        return False
+    for block in rock['blocks']:
+        x = pos[0] + block[0]
+        y = pos[1] + block[1]
+        print(x, y)
+        if pg[x][y] == 1:
+            return False
+    return True
+
+
+def buffer(height):
+    global pg
+    l = len(pg)
+    if l <= height:
+        for _ in range(height - l + 1):
+            pg.append([0] * 7)
+
+
+def print_pg(rock, pos):
+    os.system('clear')
+    new_pg = deepcopy(pg) 
+    for block in rock['blocks']:
+        x = pos[0] + block[0]
+        y = pos[1] + block[1]
+        new_pg[x][y] = 2
+
+    for idx in reversed(range(len(pg))):
+        visual_row = []
+        for p in new_pg[idx]:
+            if p == 0:
+                visual_row.append('.')
+            elif p == 1:
+                visual_row.append('#')
             else:
-                next_idx += 7
-            for v in line[next_idx:].split(","):
-                valve_adj[valve].append(v.strip())
+                visual_row.append('@')
+        print(''.join(visual_row))
 
 
-@lru_cache
-def distance(v1, v2):
-    if v1 == v2:
-        return 0
-
-    valves = list(valve_adj.keys())
-    visited = {}
-    for v in valves:
-        visited[v] = False
-
-    q = deque([(v1, 0)])
-    visited[v1] = True
-
-    while len(q) > 0:
-        cur_v, d = q.popleft()
-        visited[cur_v] = True
-        if cur_v == v2:
-            return d
-        else:
-            for v in valve_adj[cur_v]:
-                if not visited[v]:
-                    q.append((v, d + 1))
-
-
-best_flow_rate = -1
-cur_flow_rate = 0
-valves = list(valve_adj.keys())
-visited = {}
-for v in valves:
-    visited[v] = False
-
-potential_valves = [k for k, v in flow_rates.items() if v > 0]
-
-
-def move(v, t):
-    global best_flow_rate
-    global cur_flow_rate
-    visited[v] = True
-    if best_flow_rate < cur_flow_rate:
-        best_flow_rate = cur_flow_rate
-    for u in valves:
-        if u in potential_valves:
-            d = distance(v, u)
-            if not visited[u] and d + 1 < t:
-                new_t = t - d - 1
-                cur_flow_rate += flow_rates[u] * new_t
-                move(u, new_t)
-                cur_flow_rate -= flow_rates[u] * new_t
-    visited[v] = False
+def compute(m):
+    global pg
+    global max_height
+    turn = 0
+    while turn < m:
+        rock = get_rock()
+        effect = get_effect()
+        pos = get_pos()
+        print('rock', rock)
+        print('effect', effect)
+        print('pos', pos)
+        input()
+        buffer(pos[1] + rock['height'])
+        while True:
+            # effect
+            new_pos = (pos[0], pos[1] + effect)
+            if valid(rock, new_pos):
+                pos = new_pos
+            # fall
+            new_pos = (pos[0] - 1, pos[1])
+            if valid(rock, new_pos):
+                pos = new_pos
+            else:
+                for block in rock['blocks']:
+                    x = pos[0] + block[0]
+                    y = pos[1] + block[1]
+                    pg[x][y] = 1
+                max_height = max(max_height, pos[1] + rock['height'])
+                print_pg(rock, pos)
+                input()
+                break
+            print_pg(rock, pos)
+            input()
 
 
-move("AA", 30)
-print(best_flow_rate)
+print(compute(1))
